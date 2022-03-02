54b64e5e9f82cf750c3ba02380cd24afb5f3ce12
Siddhartha Roy <sid86harth@gmail.com>
2022-03-03 04:01:03 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export default NextAuth({
     providers: [
         // OAuth authentication providers...
         GoogleProvider({
-            clientId: {process.env.NEXT_PUBLIC_GOOGLE_CLIENTID},
-            clientSecret: {process.env.NEXT_PUBLIC_GOOGLE_CLIENTSECRET}
+            clientId: 'clientid',
+            clientSecret: 'clientsecret'
         }),
     ],
     adapter: FirebaseAdapter(db),
