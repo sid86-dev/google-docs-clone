6644f8b8effb2cd5dcf7e436ffb9574f17b40b28
sid86-dev <sid86harth@gmail.com>
2022-03-11 15:25:15 +0530

    Dockerize repo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
   images:{
     domains:['i.ibb.co','www.securedsigning.com'],
